4ddd064e615e122aa6e5b9e02d9a3c49231a0e3c
chihhao <Wallis@chihhaodeMacBook-Pro.local>
2019-01-12 16:52:26 +0800

    ready to pull

--- public/javascripts/manage.js
+++ public/javascripts/manage.js
@@ -1,7 +1,19 @@
+$('select.dropdown')
+  .dropdown()
+;
 const addClicked = () => {
     $('.ui.modal')
     .modal('show')
     ;
 }
 
-const add = document.getElementById('add').addEventListener('click', addClicked)
+document.getElementById('add').addEventListener('click', addClicked)
+
+const addProjectClicked = (event) => {
+    $('.ui.modal').modal({
+        onApprove : function(){return false;}
+    }).modal('show');
+}
+
+document.getElementById('add-project').addEventListener('click', addProjectClicked)
+
