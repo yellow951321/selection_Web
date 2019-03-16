39fe25bc87773295d08f2e95614071425888b128
f74064054@mail.ncku.edu.tw <f74064054@mail.ncku.edu.tw>
2019-03-16 12:54:04 +0800

    modified the graph.js

--- routes/manage/opera/graph.js
+++ routes/manage/opera/graph.js
@@ -23,57 +23,95 @@ const drawBarChart = require('./drawAPI')
 
 router.get('/',async (req,res)=>{
   try{
+  const user = await User.findOne({
+    where:{
+      user_id: req.session.userId
+    }
+  })
+  if(user == null)
+    throw new Error(`No userId ${req.session.userId}`)
+  else
+    var {dataValues, } = user
+  const result = await OP.countAllDetailRespectToCampusId(res.locals.campus_id)
+
+  const result_word = OP.convertToWord(result)
+
+  const dataSet = OP.convertToJson(result_word,{json: false})
+
+  // console.log(dataSet)
+  var fn = pug.compileFile(`views/playground.pug`)
+  const type = getFromNum(map,{type: res.locals.type_id})
+  var html = fn({
+    title: res.locals.campus,
+    GLOBAL:{
+      id: req.session.userId,
+      user: dataValues.user_name,
+      year: res.locals.year,
+      type: type,
+      campus: res.locals.campus
+    }
+  })
+  // console.log('In here')
+  const { document } = (new JSDOM(html)).window
+  // draw the graph
+  // const data = JSON.parse(fs.readFileSync('public/data/test.json'))
+
+  const { year } = (await findYearById(res.locals.year_id)).dataValues
+
+  console.log(res.locals.campus_id)
+  const { campus_name } =  (await findCampusById(res.locals.campus_id)).dataValues
+  const campus_word = getFromNum(map,{campus: campus_name, type: res.locals.type_id})
+  drawBarChart(document.querySelector('body'),dataSet,{
+    year: year,
+    year_id : res.locals.year_id,
+    type: type,
+    campus: campus_word,
+    id: req.session.userId
+  })
+  res.send(document.querySelector('html').innerHTML)
+}
+catch(err){
+  console.log(err)
+}
+
+})
+
+router.get('/', async (req,res)=>{
+  try{
+
     const user = await User.findOne({
-      where:{
+      where: {
         user_id: req.session.userId
       }
     })
-    if(user == null)
-      throw new Error(`No userId ${req.session.userId}`)
+
+    if( user == null)
+      throw new Error(`No User Id named ${req.session.userId}`)
     else
       var {dataValues, } = user
-    const result = await OP.countAllDetailRespectToCampusId(res.locals.campus_id)
 
-    const result_word = OP.convertToWord(result)
+    console.log(dataValues)
 
-    const dataSet = OP.convertToJson(result_word,{json: false})
-
-    // console.log(dataSet)
-    var fn = pug.compileFile(`views/playground.pug`)
-    const type = getFromNum(map,{type: res.locals.type_id})
-    var html = fn({
-      title: res.locals.campus,
+    res.render('manage/graph.pug',{
       GLOBAL:{
         id: req.session.userId,
         user: dataValues.user_name,
         year: res.locals.year,
-        type: type,
+        type: res.locals.type,
         campus: res.locals.campus
-      }
+      },
     })
-    // console.log('In here')
-    const { document } = (new JSDOM(html)).window
-    // draw the graph
-    // const data = JSON.parse(fs.readFileSync('public/data/test.json'))
-
-    const { year } = (await findYearById(res.locals.year_id)).dataValues
-
-    console.log(res.locals.campus_id)
-    const { campus_name } =  (await findCampusById(res.locals.campus_id)).dataValues
-    const campus_word = getFromNum(map,{campus: campus_name, type: res.locals.type_id})
-    drawBarChart(document.querySelector('body'),dataSet,{
-      year: year,
-      year_id : res.locals.year_id,
-      type: type,
-      campus: campus_word,
-      id: req.session.userId
-    })
-    res.send(document.querySelector('html').innerHTML)
-  }
-  catch(err){
+  }catch (err){
     console.log(err)
   }
+})
 
+router.get('/',async (req,res)=>{
+  try{
+
+  }catch(err){
+
+  }
 })
 
 
