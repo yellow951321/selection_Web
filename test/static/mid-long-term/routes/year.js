ea9b7784384796ff31f0440a62286b7043ab689b
F74066187 <dio23310045123@gmail.com>
2019-08-14 23:54:26 +0800

    fix label-from-number.js

--- test/static/mid-long-term/routes/year.js
+++ test/static/mid-long-term/routes/year.js
@@ -9,22 +9,52 @@ chai.use(sinonChai)
 const expect = chai.expect
 const sandbox = sinon.createSandbox();
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+import pugRender from 'pug-render'
+import config from 'projectRoot/config'
+import path from 'path'
+const midLongTermPugRoot = path.join(config.projectRoot, 'mid-long-term/views')
+
+const render = pugRender(midLongTermPugRoot, {
+    locals: {env: process.env},
+})
+
 import server from 'projectRoot/server.js'
 import yearRouter from 'mid-long-term/routes/year.js'
 
 describe('yearRouter', ()=>{
 
     context('Get /index', ()=>{
-        it('should get /index', ()=>{
+        it('should get /index', async ()=>{
             let dbStub = sinon.stub().callsFake(()=>{
                 return []
             })
             // yearRouter.__set__('getAllYear', dbStub)
-
-            request(server).get('/auth/login').set('Accept', 'application/json').then(res=>{
-                console.log(res)
-            })
-                
+            console.log(1)
+            let output = await request(server).get('/mid-long-term/0/0/index')
+            console.log(output.text)
+            // console.log(await render('year', {
+            //     breadcrumb: [
+            //       {
+            //         id: 'mid-long-term',
+            //         name: '中長程計畫',
+            //       },
+            //       {
+            //         id: res.locals.typeId,
+            //         name: typeName,
+            //       },
+            //       {
+            //         id: res.locals.campusId,
+            //         name: campusName,
+            //       }
+            //     ],
+            //     id: req.session.userId,
+            //     user: res.locals.user,
+            //     data
+            //   }))
         })
     })
 })
+
+
